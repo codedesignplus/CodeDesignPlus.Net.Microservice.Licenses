48d895d1fe3c114b7402e682a4f5fb6ee35b8839
CodeDesignPlus <codedesignsplus@outlook.com>
2025-02-02 22:00:35 -0500

    feat: Enhance Mapster configuration for license and module DTOs with command mappings

--- src/domain/CodeDesignPlus.Net.Microservice.Licenses.Application/Setup/MapsterConfig.cs
+++ src/domain/CodeDesignPlus.Net.Microservice.Licenses.Application/Setup/MapsterConfig.cs
@@ -1,8 +1,29 @@
-﻿using CodeDesignPlus.Net.Microservice.Licenses.Domain.ValueObjects;
+﻿using CodeDesignPlus.Microservice.Api.Dtos;
+using CodeDesignPlus.Net.Microservice.Licenses.Application.License.Commands.CreateLicense;
+using CodeDesignPlus.Net.Microservice.Licenses.Application.License.Commands.UpdateLicense;
+using CodeDesignPlus.Net.Microservice.Licenses.Domain.Entities;
+using CodeDesignPlus.Net.Microservice.Licenses.Domain.ValueObjects;
 
 namespace CodeDesignPlus.Net.Microservice.Licenses.Application.Setup;
 
 public static class MapsterConfigLicense
 {
-    public static void Configure() { }
+    public static void Configure()
+    {
+        //License
+        TypeAdapterConfig<CreateLicenseDto, CreateLicenseCommand>
+            .NewConfig()
+            .ConstructUsing(src => new CreateLicenseCommand(src.Id, src.Name, src.Description, src.Modules, src.BillingType, Currency.Create(src.Currency.Name, src.Currency.Code, src.Currency.Symbol), src.Price, src.Attributes));
+
+        TypeAdapterConfig<UpdateLicenseDto, UpdateLicenseCommand>
+            .NewConfig()
+            .ConstructUsing(src => new UpdateLicenseCommand(src.Id, src.Name, src.Description, src.Modules, src.BillingType, Currency.Create(src.Currency.Name, src.Currency.Code, src.Currency.Symbol), src.Price, src.Attributes));
+
+        TypeAdapterConfig<LicenseAggregate, LicenseDto>.NewConfig();
+
+        //Module
+        TypeAdapterConfig<ModuleDto, ModuleEntity>
+            .NewConfig()
+            .TwoWays();
+    }
 }
