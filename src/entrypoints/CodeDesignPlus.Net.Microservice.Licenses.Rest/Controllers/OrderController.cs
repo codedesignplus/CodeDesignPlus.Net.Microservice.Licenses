f79538d2563d933c0dcc578ae67b4241804ea158
Wilzon Liscano <wliscano@codedesignplus.com>
2025-06-28 06:12:01 -0500

    feat: add UpdateStateOrder endpoint to OrderController with appropriate response types

--- src/entrypoints/CodeDesignPlus.Net.Microservice.Licenses.Rest/Controllers/OrderController.cs
+++ src/entrypoints/CodeDesignPlus.Net.Microservice.Licenses.Rest/Controllers/OrderController.cs
@@ -1,3 +1,5 @@
+using CodeDesignPlus.Net.Microservice.Licenses.Domain.ValueObjects;
+
 namespace CodeDesignPlus.Net.Microservice.Licenses.Rest.Controllers;
 
 /// <summary>
@@ -27,6 +29,11 @@ public class OrderController(IMediator mediator, IMapper mapper) : ControllerBas
     /// <returns>HTTP status code 204 (No Content).</returns>
     [HttpPost("{id}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ProblemDetails))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ProblemDetails))]
     public async Task<IActionResult> PayOrder(Guid id, [FromBody] PayOrderDto data, CancellationToken cancellationToken)
     {
         data.Id = id;
@@ -34,6 +41,29 @@ public class OrderController(IMediator mediator, IMapper mapper) : ControllerBas
         await mediator.Send(mapper.Map<PayOrderCommand>(data), cancellationToken);
 
         return NoContent();
+    }
+
+    /// <summary>
+    /// Update the state of an Order by its ID.
+    /// </summary>
+    /// <param name="id">The unique identifier of the Order.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <response code="200">Returns the updated PaymentResponse.</response>
+    /// <response code="400">If the request is invalid.</response>
+    /// <response code="401">If the user is not authenticated.</response>
+    /// <response code="403">If the user does not have permission to update the Order.</response>
+    /// <response code="500">If an internal server error occurs.</response>
+    [HttpPut("{id}/state")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(PaymentResponse))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ProblemDetails))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ProblemDetails))]
+    public async Task<IActionResult> UpdateStateOrder(Guid id, CancellationToken cancellationToken)
+    {
+        var result = await mediator.Send(new UpdateStateOrderCommand(id), cancellationToken);
+
+        return Ok(result);
     }
 }
 
